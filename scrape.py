1d820d4104e666da15816f6777f53314fd0e12ec
Conway <yawnoc@pm.me>
2022-04-05 21:55:17 +0800

    Add episode counts

---
+++ scrape.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+# scrape.py
+
+Scraping images and captions from <https://frinkiac.com>.
+"""
+
+
+EPISODE_COUNT_FROM_SEASON = {
+  1: 13,
+  2: 22,
+  3: 24,
+  4: 22,
+  5: 22,
+  6: 25,
+  7: 25,
+  8: 25,
+  9: 25,
+  10: 23,
+  11: 22,
+  12: 21,
+  13: 22,
+  14: 22,
+  15: 22,
+  16: 21,
+  17: 22,
+  18: 22,
+  19: 20,
+}
